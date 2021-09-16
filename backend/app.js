58545bf9f82a104be1452c0a21ece54cd5ee901e
Aiman <aiman.ahmedpro@gmail.com>
2021-09-16 22:44:38 +0200

    Chamgement caractere

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,14 @@ const helmet = require('helmet');
 require('dotenv').config();
 
   const app = express();
-  
+   app.use(helmet());  // permet de proteger l'application de certaines vulnerabilités bien connues du web en configurant de manière appropriés les entetes HTTP
   app.use((req, res, next) => { // permet de regler  le problème de CORS (Cross Origin Resource Sharing)
     res.setHeader('Access-Control-Allow-Origin', '*'); // permet à tout le monde d'acceder à l'API
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // on donne l'autorisation d'utiliser certains Headers
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // on donne l'autorisation d'utiliser certaines methodes
     next();
   });
-  app.use(helmet());  // permet de proteger l'application de certaines vulnerabilités bien connues du web en configurant de manière appropriés les entetes HTTP
+ 
   app.use(express.json()); // transforme le corps de la requete post en JSON 
   app.use(xss()); // permet d'empecher l'utilisation cross site scripting
   app.use('/images', express.static(path.join(__dirname, 'images'))); // Permet que les requetes à /images/ servent le dossier images
